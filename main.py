966c1c75413284e768a8b55591e41d4b86b5db38
Drangleiker1 <bartosz.radomski@onet.pl>
2022-12-13 19:47:21 +0100

    Hi. Welcome in Hell

--- main.py
+++ main.py
@@ -1,14 +1,34 @@
 from tkinter import *
+from tkinter import filedialog as fd
+
+def add_image():
+    filetypes = (
+        ('Obrazki PNG', '*.png'),
+        ('All files', '*.*')
+    )
+    fd.askopenfilename(filetypes=filetypes)
 
 window = Tk()
+window.minsize(640, 480)
+window.minsize(640, 480)
+window.geometry("640x480")
 
-window.maxsize(640,400)
-window.minsize(640,400)
+photo = PhotoImage(file="Cornwall Cav.png")
 
-photo=PhotoImage(file="Cornwall Cav.png")
+frame1 = Frame(window, bg="black", height= 50)
+frame1.pack()
 
-label1 = Frame(window, width = 640, height = 400, bd=3,bg="black")
-mainFrame = Label(window, text="Witam", font = ("Arial", 24), bd=10, bg="black", fg ="white", image=photo, compound="top")
+addButton = Button(frame1, command=add_image, text = "Dodaj obrazek", font = ("Arial", 24), )
+addButton.pack()
 
-mainFrame.pack()
+mainLabel= Label(window,
+                  text="Moja pierwsza gra",
+                  font = ("Arial", 28),
+                  bd=10,
+                  bg="black",
+                  fg="white",
+                  image=photo,
+                  compound="top")
+
+mainLabel.pack()
 window.mainloop()
