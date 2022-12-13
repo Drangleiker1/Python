b5d94fbedc19a77086f1ed83edbd9a8e25d5e847
Drangleiker1 <bartosz.radomski@onet.pl>
2022-12-13 19:06:57 +0100

    Hiwdadad

--- main.py
+++ main.py
@@ -2,7 +2,13 @@ from tkinter import *
 
 window = Tk()
 
-window.maxsize(600,300)
-window.minsize(600,300)
+window.maxsize(640,400)
+window.minsize(640,400)
 
+photo=PhotoImage(file="Cornwall Cav.png")
+
+label1 = Frame(window, width = 640, height = 400, bd=3,bg="black")
+mainFrame = Label(window, text="Witam", font = ("Arial", 24), bd=10, bg="black", fg ="white", image=photo, compound="top")
+
+mainFrame.pack()
 window.mainloop()
